b25f9726965b1dfd446c1b42b9df20bfb7d9595f
AlliysonBonner <alliyson.bonner@gmail.com>
2023-02-27 12:49:43 -0600

    Initial commit of Python Image Manipulation project.  This commit includes the 'transform.py' module which provides functions for performing various image manipulations such as brightness adjustment, contrast adjustment, and blurring using the Numpy package. The module also includes a method for combining two images using the squared sum of squares. The 'image.py' module is used to load and save images in various formats.  This project demonstrates my proficiency in Python and Numpy, and showcases my ability to apply these skills to manipulate images effectively.

--- README.md
+++ README.md
@@ -1,12 +1,45 @@
-# PyPhotoshop
-Python implementation of image filters
 
-Use Python to adjust brightness and contrast, add blur, and detect edges! Follow along tutorial: https://youtu.be/4ifdUQmZqhM
+**Oirginal image**
+![a collage of proccessed images from the project](https://github.com/AlliysonBonner/Minesweeper/blob/main/image.png?raw=true)
+This project demonstrates how to perform various image manipulations using Python. The project includes a Python module transform.py which provides the following functions:
+**Oirginal image**
 
-In order to download this code, either click the green button at the top right and download as ZIP, or use `git clone https://github.com/kying18/pyphotoshop.git`. You will need to `pip install -r requirements.txt` (or use `pip3` if you are getting a module not found error).
+* `brighten(image, factor): brightens the input image by the given factor.
+* adjust_contrast(image, factor, mid): adjusts the contrast of the input image by increasing or decreasing the difference from the given midpoint by the given factor.
+* blur(image, kernel_size): applies a blur effect to the input image using the given kernel size.
+* apply_kernel(image, kernel): applies the given kernel to the input image.
+* combine_images(image1, image2): combines two images using the squared sum of squares.
+The transform.py module uses the Image class from the image.py module to read and write image files. The Image class provides methods to load and save images in various formats.
+## Requirements
+This project requires Python 3 and the following Python packages:
+* numpy
+* imageio
+## Installation
+To install the required packages, run the following command:
+`pip install -r requirements.txt`
+## Usage
+To use the transform.py module, import the required functions and create an instance of the Image class with the input image file:
+```
+from image import Image
+from transform import brighten, adjust_contrast, blur, apply_kernel, combine_images
 
-In the folder, you will find these files:
-- image.py: contains the `Image` class that will read and write the images using the PNG `Writer` and `Reader`
-- png.py: pure Python PNG `Reader` and `Writer` classes from Johann C. Rocholl
-- transform.py: implemented image filter functions
-- transform_empty.py: empty template for image filter functions
+image = Image(filename='input.png')
+
+brightened_im = brighten(image, 1.7)
+brightened_im.write_image('brightened.png')
+
+incr_contrast = adjust_contrast(image, 2, 0.5)
+incr_contrast.write_image('increased_contrast.png')
+
+blur_3 = blur(image, 3)
+blur_3.write_image('blur_k3.png')
+
+sobel_x = apply_kernel(image, np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]]))
+sobel_y = apply_kernel(image, np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]]))
+sobel_xy = combine_images(sobel_x, sobel_y)
+sobel_xy.write_image('edge_xy.png')
+```
+The above code loads an image from a file named input.png and performs various transformations on it using the transform.py module. The resulting images are saved to files in the current directory.
+
+## License
+This project is licensed under the MIT License. See the LICENSE file for more details.
--- transform.py
+++ transform.py
@@ -1,117 +1,142 @@
-"""
-Python Image Manipulation by Kylie Ying (modified from MIT 6.865)
-
-YouTube Kylie Ying: https://www.youtube.com/ycubed 
-Twitch KylieYing: https://www.twitch.tv/kylieying 
-Twitter @kylieyying: https://twitter.com/kylieyying 
-Instagram @kylieyying: https://www.instagram.com/kylieyying/ 
-Website: https://www.kylieying.com
-Github: https://www.github.com/kying18 
-Programmer Beast Mode Spotify playlist: https://open.spotify.com/playlist/4Akns5EUb3gzmlXIdsJkPs?si=qGc4ubKRRYmPHAJAIrCxVQ 
-"""
-
 from image import Image
 import numpy as np
 
-def brighten(image, factor):
-    # when we brighten, we just want to make each channel higher by some amount 
-    # factor is a value > 0, how much you want to brighten the image by (< 1 = darken, > 1 = brighten)
-    x_pixels, y_pixels, num_channels = image.array.shape  # represents x, y pixels of image, # channels (R, G, B)
-    new_im = Image(x_pixels=x_pixels, y_pixels=y_pixels, num_channels=num_channels)  # making a new array to copy values to!
+def brighten(image: Image, factor: float) -> Image:
+    """
+    Brightens the input image by multiplying each channel with the given factor.
 
-    # # this is the non vectorized version
-    # for x in range(x_pixels):
-    #     for y in range(y_pixels):
-    #         for c in range(num_channels):
-    #             new_im.array[x, y, c] = image.array[x, y, c] * factor
+    Args:
+        image: An instance of the Image class representing the input image.
+        factor: A float value representing the amount to brighten the image by.
 
-    # faster version that leverages numpy
+    Returns:
+        An instance of the Image class representing the brightened image.
+    """
+    new_im = Image(x_pixels=image.x_pixels, y_pixels=image.y_pixels, num_channels=image.num_channels)
+    # Vectorized version that leverages NumPy for faster computation.
     new_im.array = image.array * factor
-
     return new_im
 
-def adjust_contrast(image, factor, mid):
-    # adjust the contrast by increasing the difference from the user-defined midpoint by factor amount
-    x_pixels, y_pixels, num_channels = image.array.shape  # represents x, y pixels of image, # channels (R, G, B)
-    new_im = Image(x_pixels=x_pixels, y_pixels=y_pixels, num_channels=num_channels)  # making a new array to copy values to!
-    for x in range(x_pixels):
-        for y in range(y_pixels):
-            for c in range(num_channels):
+def adjust_contrast(image: Image, factor: float, mid: float) -> Image:
+    """
+    Adjusts the contrast of the input image by increasing the difference from the user-defined midpoint by factor amount.
+
+    Args:
+        image: An instance of the Image class representing the input image.
+        factor: A float value representing the amount to increase the difference from the midpoint by.
+        mid: A float value representing the user-defined midpoint.
+
+    Returns:
+        An instance of the Image class representing the contrast-adjusted image.
+    """
+    new_im = Image(x_pixels=image.x_pixels, y_pixels=image.y_pixels, num_channels=image.num_channels)
+    for x in range(image.x_pixels):
+        for y in range(image.y_pixels):
+            for c in range(image.num_channels):
                 new_im.array[x, y, c] = (image.array[x, y, c] - mid) * factor + mid
-
     return new_im
 
-def blur(image, kernel_size):
-    # kernel size is the number of pixels to take into account when applying the blur
-    # (ie kernel_size = 3 would be neighbors to the left/right, top/bottom, and diagonals)
-    # kernel size should always be an *odd* number
-    x_pixels, y_pixels, num_channels = image.array.shape  # represents x, y pixels of image, # channels (R, G, B)
-    new_im = Image(x_pixels=x_pixels, y_pixels=y_pixels, num_channels=num_channels)  # making a new array to copy values to!
-    neighbor_range = kernel_size // 2  # this is a variable that tells us how many neighbors we actually look at (ie for a kernel of 3, this value should be 1)
-    for x in range(x_pixels):
-        for y in range(y_pixels):
-            for c in range(num_channels):
-                # we are going to use a naive implementation of iterating through each neighbor and summing
-                # there are faster implementations where you can use memoization, but this is the most straightforward for a beginner to understand
+def blur(image: Image, kernel_size: int) -> Image:
+    """
+    Applies a blur effect on the input image by taking into account a number of neighboring pixels determined by the given kernel size.
+
+    Args:
+        image: An instance of the Image class representing the input image.
+        kernel_size: An integer value representing the number of pixels to take into account when applying the blur.
+
+    Returns:
+        An instance of the Image class representing the blurred image.
+    """
+    new_im = Image(x_pixels=image.x_pixels, y_pixels=image.y_pixels, num_channels=image.num_channels)
+    neighbor_range = kernel_size // 2
+    for x in range(image.x_pixels):
+        for y in range(image.y_pixels):
+            for c in range(image.num_channels):
                 total = 0
-                for x_i in range(max(0,x-neighbor_range), min(new_im.x_pixels-1, x+neighbor_range)+1):
-                    for y_i in range(max(0,y-neighbor_range), min(new_im.y_pixels-1, y+neighbor_range)+1):
+                for x_i in range(max(0, x-neighbor_range), min(new_im.x_pixels-1, x+neighbor_range)+1):
+                    for y_i in range(max(0, y-neighbor_range), min(new_im.y_pixels-1, y+neighbor_range)+1):
                         total += image.array[x_i, y_i, c]
                 new_im.array[x, y, c] = total / (kernel_size ** 2)
     return new_im
 
-def apply_kernel(image, kernel):
-    # the kernel should be a 2D array that represents the kernel we'll use!
-    # for the sake of simiplicity of this implementation, let's assume that the kernel is SQUARE
-    # for example the sobel x kernel (detecting horizontal edges) is as follows:
-    # [1 0 -1]
-    # [2 0 -2]
-    # [1 0 -1]
-    x_pixels, y_pixels, num_channels = image.array.shape  # represents x, y pixels of image, # channels (R, G, B)
-    new_im = Image(x_pixels=x_pixels, y_pixels=y_pixels, num_channels=num_channels)  # making a new array to copy values to!
-    neighbor_range = kernel.shape[0] // 2  # this is a variable that tells us how many neighbors we actually look at (ie for a 3x3 kernel, this value should be 1)
+def apply_kernel(image: Image, kernel: np.ndarray) -> Image:
+    """Applies a 2D convolution kernel to an image.
+
+    Args:
+        image (Image): The image to apply the kernel to.
+        kernel (np.ndarray): The 2D convolution kernel.
+
+    Returns:
+        Image: The image after the kernel has been applied.
+    """
+    x_pixels, y_pixels, num_channels = image.array.shape
+    # Create a new Image object to store the output
+    new_im = Image(x_pixels=x_pixels, y_pixels=y_pixels, num_channels=num_channels)
+
+    # Calculate the range of neighboring pixels to consider based on the kernel size
+    neighbor_range = kernel.shape[0] // 2
+
+    # Iterate over each pixel in the image
     for x in range(x_pixels):
         for y in range(y_pixels):
             for c in range(num_channels):
                 total = 0
+                # Iterate over the neighboring pixels within the range defined by the kernel size
                 for x_i in range(max(0,x-neighbor_range), min(new_im.x_pixels-1, x+neighbor_range)+1):
                     for y_i in range(max(0,y-neighbor_range), min(new_im.y_pixels-1, y+neighbor_range)+1):
                         x_k = x_i + neighbor_range - x
                         y_k = y_i + neighbor_range - y
                         kernel_val = kernel[x_k, y_k]
+                        # Apply the kernel to the pixel and add to the total
                         total += image.array[x_i, y_i, c] * kernel_val
+                # Store the result in the output image
                 new_im.array[x, y, c] = total
+
     return new_im
 
-def combine_images(image1, image2):
-    # let's combine two images using the squared sum of squares: value = sqrt(value_1**2, value_2**2)
-    # size of image1 and image2 MUST be the same
-    x_pixels, y_pixels, num_channels = image1.array.shape  # represents x, y pixels of image, # channels (R, G, B)
-    new_im = Image(x_pixels=x_pixels, y_pixels=y_pixels, num_channels=num_channels)  # making a new array to copy values to!
+def combine_images(image1: Image, image2: Image) -> Image:
+    """Combines two images by taking the squared sum of squares.
+
+    Args:
+        image1 (Image): The first image.
+        image2 (Image): The second image.
+
+    Returns:
+        Image: The combined image.
+    """
+    assert image1.array.shape == image2.array.shape, "Images must be the same size."
+
+    x_pixels, y_pixels, num_channels = image1.array.shape
+    # Create a new Image object to store the output
+    new_im = Image(x_pixels=x_pixels, y_pixels=y_pixels, num_channels=num_channels)
+
+    # Iterate over each pixel in the images
     for x in range(x_pixels):
         for y in range(y_pixels):
             for c in range(num_channels):
+                # Calculate the squared sum of squares and take the square root
                 new_im.array[x, y, c] = (image1.array[x, y, c]**2 + image2.array[x, y, c]**2)**0.5
+
     return new_im
-    
+
 if __name__ == '__main__':
-    lake = Image(filename='lake.png')
+    cake = Image(filename='cake.png')
     city = Image(filename='city.png')
 
     # brightening
-    brightened_im = brighten(lake, 1.7)
+    brightened_im = brighten(cake, 1.7)
     brightened_im.write_image('brightened.png')
 
     # darkening
-    darkened_im = brighten(lake, 0.3)
+    darkened_im = brighten(cake, 0.3)
     darkened_im.write_image('darkened.png')
 
     # increase contrast
-    incr_contrast = adjust_contrast(lake, 2, 0.5)
+    incr_contrast = adjust_contrast(cake, 2, 0.5)
     incr_contrast.write_image('increased_contrast.png')
 
     # decrease contrast
-    decr_contrast = adjust_contrast(lake, 0.5, 0.5)
+    decr_contrast = adjust_contrast(cake, 0.5, 0.5)
     decr_contrast.write_image('decreased_contrast.png')
 
     # blur using kernel 3
@@ -131,4 +156,3 @@ if __name__ == '__main__':
     # let's combine these and make an edge detector!
     sobel_xy = combine_images(sobel_x, sobel_y)
     sobel_xy.write_image('edge_xy.png')
-
